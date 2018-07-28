10dfd59bb40de4157a1eea3dc8819a62282432f5
Atuma <atuma.daniel@yahoo.com>
2018-07-28 08:58:45 +0100

    First commit

---
+++ app/src/main/java/com/example/root/quiko/PlansListActivity.java
@@ -0,0 +1,15 @@
+package com.example.root.quiko;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlansListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plans_list);
+
+
+    }
+}
